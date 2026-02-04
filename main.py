bdd7bf637b9bc14798b8debe314ff141077d98be
Jakob Smith <jakobrs0601@gmail.com>
2026-02-04 14:16:46 -0600

    added options to sort by default or just by extention, and merged make_dir() with sort.

--- main.py
+++ main.py
@@ -12,42 +12,47 @@ types = {
     "Documents": [".doc", ".docx", ".odt", ".xls", ".xlsx", ".ods", ".ppt", ".pptx", ".odp"],
     "Font": [".woff", ".woff2", ".ttf", ".otf"],
     "Application": [".wasm", ".exe"],
-    "Folders": []
 }
+
+def sort_by():
+    #types['Images'].append(".pixel")
+    print("Default Catagories:")
+    for type in types:
+        print(type)
+        print(types.get(type))
+    print("(By Default Folders and Others each get their own catagories)\n")
+    while True:
+        response = input("\nHow would you like to sort your files? \n 1. Default \n 2. By Extention\n\n")
+        if response == "1":
+              print("Default\n")
+              return 1
+        if response == "2":
+              print("By Extention\n")
+              return 2
+        #if response == "3":
+              #print("Custom\n")
+              #return 1
+        
 
 def sort_to():
     p = Path.cwd()
-    sort_to = input(f'Where do you want to sort to? (leave blank for default location "{p.name}") ').strip()
+    sort_to = input(f'Where do you want to sort to? (leave blank for default location "{p.name}")\n').strip()
     if sort_to != p and sort_to != '':
           p = sort_to
     return Path(p)
 
-def make_dirs(p):
-    path = p / "Sorted"
-    if not path.is_dir():
-        path.mkdir()
-    for type in types:
-        path = p / "Sorted" / type
-        if not path.is_dir():
-            path.mkdir()
-    path = p / "Sorted/Others"
-    if not path.is_dir():
-            path.mkdir()
-    path = p / "Sorted/Folders"
-    if not path.is_dir():
-            path.mkdir()
-    print("Directories created!")
-
-    
 def sort_from():
     f = Path.home()/"Downloads"
-    sort_to = input(f'Where do you want to sort? (leave blank for default location "{f.name}") ').strip()
+    sort_to = input(f'\nWhere do you want to sort? (leave blank for default location "{f.name}")\n').strip()
     if sort_to != f and sort_to != '':
         f = sort_to
     return Path(f)
 
-def sort(f,p):
+def sort_1(f,p):
     p = p/"Sorted"
+    if not p.is_dir():
+        p.mkdir()
+        print("Sorted Folder created")
     for file in f.iterdir():
         if file.is_dir():
             shutil.move(file, p/"Folders")
@@ -56,15 +61,46 @@ def sort(f,p):
         found = False
         for type in types:
             if fs in types[type]:
-                 shutil.move(file, p/type)
-                 found = True
-                 break
+                path = p / type
+                if not path.is_dir():
+                    path.mkdir()
+                    print(type, "Folder created")
+                shutil.move(file, p/type)
+                print(file.name, "sorted into", type)
+                found = True
+                break
         if found == False:
+            path = p / "Others"
+            if not path.is_dir():
+                    path.mkdir()
+                    print("Others Folder created")
             shutil.move(file, p/"Others")
+            print(file.name, "sorted into Others")
     print("Files Sorted!")
 
+def sort_2(f,p):
+    p = p/"Sorted"
+    if not p.is_dir():
+        p.mkdir()
+        print("Sorted Folder created")
+    for file in f.iterdir():
+        if file.is_dir():
+            shutil.move(file, p/"Folders")
+            continue
+        fs = file.suffix
+        path = p / fs
+        if not path.is_dir():
+                    path.mkdir()
+                    print(fs, "Folder created")
+        shutil.move(file, p/fs)
+        print(file.name, "sorted into", fs)
+    print("Files Sorted!")
      
 f = sort_from()
 p = sort_to()
-make_dirs(p)
-sort(f,p)
+sm = sort_by()
+if sm == 1:
+     sort_1(f,p)
+elif sm == 2:
+     sort_2(f,p)
+     print("WIP")
